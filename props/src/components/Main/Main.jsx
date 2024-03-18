e36fe51a3bff600b30955984c4e234d92d54bfdb
PredoH1 <pedroh2006candido@gmail.com>
2024-03-18 09:55:01 -0300

    Feat: terminei a primeira parte da main

--- props/src/components/Main/Main.jsx
+++ props/src/components/Main/Main.jsx
@@ -1,16 +1,30 @@
 import bigmac from "../../assets/bigmac.svg";
+import bigmac2 from "../../assets/bigmac.svg";
+import sorvete from "../../assets/sorvete.svg";
+import batata from "../../assets/batata.svg";
+
 import "./Main.css";
 
 function Main() {
   return (
     <>
       <main>
-        <div className="container">
-          <img src={bigmac} alt="" />
-          <h1>
-            BATEU AQUELA <span>#FOME</span> DE <span>MÉQUI?</span>
-          </h1>
-        </div>
+        <section className="container">
+          <div className="top">
+            <img src={bigmac} alt="" />
+            <h1>
+              BATEU AQUELA <span>#FOME</span> DE <span>MÉQUI?</span>
+            </h1>
+          </div>
+          <div className="lanches">
+            <img className="bigmac" src={bigmac2} alt="" />
+            <img src={batata} alt="" />
+            <img src={sorvete} alt="" />
+          </div>
+        </section>
+        <section className="container2">
+          <h1>Promoção</h1>
+        </section>
       </main>
     </>
   );
